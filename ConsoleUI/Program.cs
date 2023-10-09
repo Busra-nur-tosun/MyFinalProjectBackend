3e8778ae8b5de2e57af7410362f7ec8e20b241db
Busra-nur-tosun <74898825+Busra-nur-tosun@users.noreply.github.com>
2023-10-09 19:46:40 +0300

    C# Kurumsal Yazılım Mimarileri

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -19,9 +19,9 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetByUnitPrice(50, 100))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName+"/"+product.CategoryName);
             }
         }
     }
