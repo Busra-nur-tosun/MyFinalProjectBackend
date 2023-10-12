0c8445d95dd05012afded3f105fdd2598d341766
Busra-nur-tosun <74898825+Busra-nur-tosun@users.noreply.github.com>
2023-10-12 16:52:49 +0300

    10.gün video

--- Core/Utilities/Results/DataResult.cs
+++ Core/Utilities/Results/DataResult.cs
@@ -1,33 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.Utilities.Results
 {
     public class DataResult<T> : Result, IDataResult<T>
     {
-        private List<global::Entities.Concrete.Product> data;
-        private bool success;
-        private string message;
-
-        public DataResult(T data,bool success,string message):base(success,message)
-        {
-            Data = data;
-        }
-        public DataResult(T data,bool success):base(success)
+        public DataResult(T data, bool success, string message) : base(success, message)
         {
             Data = data;
         }
 
-        public DataResult(List<global::Entities.Concrete.Product> data, bool success, string message)
+        public DataResult(T data, bool success) : base(success)
         {
-            this.data = data;
-            this.success = success;
-            this.message = message;
+            Data = data;
         }
-
-        public T Data {get;}
+        public T Data { get; }
     }
 }
