472b3c3dcc86c033e5432b3add1043afab32dfbc
Busra-nur-tosun <74898825+Busra-nur-tosun@users.noreply.github.com>
2023-12-08 14:07:30 +0300

    Core katmanına expextion  klasörüne expextionMidleware ve expectionmidlewareexpection yazılarak  startup da çağrılıd.20. video

---
+++ Core/Extentions/ErrorDetails.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Core.Extensions
+{
+    internal class ErrorDetails
+    {
+      
+            public string Message { get; set; }
+            public int StatusCode { get; set; }
+
+            public override string ToString()
+            {
+                return JsonConvert.SerializeObject(this);
+            }
+        
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -79,6 +79,7 @@ namespace WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
+            app.ConfigureCustomExceptionMiddleware();
             app.UseCors(builder=>builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             app.UseHttpsRedirection();
 
