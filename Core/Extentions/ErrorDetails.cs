1e687c03468c315865f66e05e69314b8cf7336bd
Busra-nur-tosun <74898825+Busra-nur-tosun@users.noreply.github.com>
2023-12-13 15:47:18 +0300

    21. video sonu

--- Core/Extentions/ErrorDetails.cs
+++ Core/Extentions/ErrorDetails.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using FluentValidation.Results;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Core.Extensions
 {
@@ -7,11 +9,16 @@ namespace Core.Extensions
       
             public string Message { get; set; }
             public int StatusCode { get; set; }
+        public IEnumerable<ValidationFailure> Errors { get; internal set; }
 
-            public override string ToString()
+        public override string ToString()
             {
                 return JsonConvert.SerializeObject(this);
             }
         
+        public class ValidationErrorDetails:ErrorDetails
+        {
+            public IEnumerable<ValidationFailure> Errors { get; set; }
+        }
     }
 }
